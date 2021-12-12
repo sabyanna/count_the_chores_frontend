d77bb6fe353ad80edb3c9618be73df8b4ba8cac7
sabyanna <anna.sabransky@cx-ray.com>
2021-12-12 13:54:33 +0100

    initial setup

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import sytles from '../styles/Layout.module.ss'
+
+const Layout = () => {
+  return (
+    <div className={ stlyes.container }>
+      main
+    </div>
+  )
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Count your chores</title>
+        <meta name='keywords' content='chores, counter' />
+      </Head>
+      <h1>Welcome to my app</h1>
+    </>
+  )
+}
